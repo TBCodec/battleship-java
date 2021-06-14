275af3f8e48c64bab8696aa49dcd4bfdfbc50bab
appuser <appuser@journey2-76cd86c789-m9z6v>
2021-06-14 09:28:23 +0000

    starter project

---
+++ src/main/java/com/codecool/battleship/Battleship.java
@@ -0,0 +1,12 @@
+package com.codecool.battleship;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Battleship {
+
+    public static void main(String[] args) {
+        // TODO Implement the game here
+    }
+
+}
