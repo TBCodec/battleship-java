05ad262aa547598f5e39c26f956d3b65e6aef7df
Frev0511 <69258208+Frev0511@users.noreply.github.com>
2021-06-15 09:12:12 +0200

    Update Battleship.java
    The creater and the printer is created.

--- src/main/java/com/codecool/battleship/Battleship.java
+++ src/main/java/com/codecool/battleship/Battleship.java
@@ -5,26 +5,24 @@ import java.util.Scanner;
 
 public class Battleship {
     //placement phase method goes here
+    public static char[][] map = new char[5][5];
+    public static char[][] test = new char[8][4];
+    public static char[][] test2 = new char[10][10];
 
     public static char InputChar(String message) {
         //message method that asks for an input and then returns it
         return 'a';
     }
-// static Player1Phase {
+    // static Player1Phase {
     public static void main(String[] args) {
-        int[] RowNames = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        char[] ColoumNames = {'A', 'B', 'C', 'D', 'E'};
-
         System.out.println("Player 1, its your turn");
-        char[][] map = new char[5][5];
-        for (int i = 0; i < map.length; i++) {
-            System.out.println();
-            for (int j = 0; j < map.length; j++) {
-                if (i == 3 && j == 3) map[i][j] = 'x';
-                System.out.print(map[i][j] + "  ");
-            }
+        /*createMatrix(map);
+        printMatrix(map);
+        createMatrix(test);
+        printMatrix(test);
+        createMatrix(test2);
+        printMatrix(test2);*/
 
-        }
         System.out.println();
         Scanner input = new Scanner(System.in);
         System.out.println("Set the row, coloumn and  direction");
@@ -41,55 +39,32 @@ public class Battleship {
 
     }
 
-
-public static void main(String args) {
-    Int[] RowNames = return_Array();
-    char[] ColumnNames= return_Column();
-
-
-
-    System.out.println("Player 2, its your turn");
-    char[][][] map = new char[PlayerNumber][5][5];
-    for (int 0 = Playernumber; i < map.length ;
-    i++){
-        System.out.println();
-        for (int j = 0; j < map.length; j++) {
-            if (i == 3 && j == 3) map[PlayerNumber][i][j] = 'x';
-            System.out.print(map[PlayerNumber][i][j] + "  ");
+    public static void createMatrix(char[][] board){
+        for(int i = 0; i < board.length; i ++){
+            for(int j = 0; j < board[i].length; j ++) {
+                board[i][j] = '0';
+            }
         }
     }
-        char PlayerNumber() {
-         Playernumber= 0;}
 
-
-//user enters row, coloumns and direction commands and the game processes those commands
-        /*System.out.println("Set the row, coloumn and  direction");
-        String RowInput=input.nextLine();
-        String ColoumnInput=input.nextLine();
-        String DirectionInput=input.nextLine();
-
-        int ColoumnInputNumber=Integer.parseInt(ColoumnInput);
-
-
+    public static void printMatrix(char[][] board){
+        String backSpaces = "  ";
+        int[] rowNames = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        char[] coloumNames = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+        for(int i = 0; i < board[0].length; i ++) {
+            if(i == 0) System.out.print("   "+rowNames[i] + backSpaces);
+            else System.out.print(+rowNames[i] + backSpaces);
         }
+        System.out.println();
+        for(int i = 0; i < board.length; i ++){
+            for (int j = 0; j < board[i].length; j ++){
+                if(j == 0){
+                    System.out.print(coloumNames[i]+backSpaces+board[i][j]+backSpaces);
+                }
+                else System.out.print(board[i][j]+backSpaces);
+            }
+            System.out.println();
         }
-        }
-
-
-
-         //String UserInput = Input.nextLine();
-
-        //validate information
-        //char reading method
-        //for( int map =0; map <;i++);
-        //while (map>5 && map <1){
-                //System.out.println("Set the row, coloumn and  direction");
-                //if the choice is not correct, display an error message saying that the choices arent correct
-
-
-
-
-//Shooting phase
-
-
-
+        System.out.println();
+    }
+}
