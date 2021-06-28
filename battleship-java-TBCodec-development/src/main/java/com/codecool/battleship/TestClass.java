f173c23d3b9ee1055b49aa456e107938bdda91f3
unknown <krisinagy@hotmail.com>
2021-06-28 14:31:30 +0200

    alapséma kész

---
+++ battleship-java-TBCodec-development/src/main/java/com/codecool/battleship/TestClass.java
@@ -0,0 +1,13 @@
+package com.codecool.battleship;
+
+public class TestClass {
+
+
+    public static char[][] map1 = new char[][]{
+            {'0', '0', '0', '0', '0'},
+            {'0', '0', '0', '0', '0'},
+            {'0', '0', 'X', '0', '0'},
+            {'0', '0', '0', '0', '0'},
+            {'0', '0', '0', '0', '0'}
+    };
+}
